9e83b73475f57596c0a159cb4b02341c2e1c5284
san ats <san@gmail.com>
2025-01-12 16:20:44 -0500

    Modelos relacionados

---
+++ Backend/api/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Piso, Habitacion, Dispositivo , RegistroConsumo
+
+@admin.register(Piso)
+class PisoAdmin(admin.ModelAdmin):
+    list_display = ('nivel', 'fecha_actualizacion')
+
+
+@admin.register(Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'piso', 'fecha_actualizacion')
+
+
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'habitacion', 'consumo_actual', 'apagado_remoto', 'fecha_actualizacion')
+
+@admin.register(RegistroConsumo)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ('dispositivo','consumo','fecha')
---
+++ Backend/api/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+class Piso(models.Model):
+    nivel = models.PositiveIntegerField()
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Piso {self.nivel}"
+
+
+class Habitacion(models.Model):
+    piso = models.ForeignKey(Piso, on_delete=models.CASCADE, related_name="habitaciones")
+    numero = models.PositiveIntegerField()
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Habitación {self.numero} - Piso {self.piso.nivel}"
+
+
+class Dispositivo(models.Model):
+    TIPOS_DISPOSITIVO = [
+        ('AIRE', 'Aire Acondicionado'),
+        ('VENTILADOR', 'Ventilador'),
+        ('TELEVISOR', 'Televisor'),
+        ('FOCO_HABITACION', 'Foco Habitación'),
+        ('FOCO_BANO', 'Foco Baño'),
+    ]
+
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE, related_name="dispositivos")
+    tipo = models.CharField(max_length=50, choices=TIPOS_DISPOSITIVO)
+    consumo_actual = models.FloatField(default=0.0)
+    apagado_remoto = models.BooleanField(default=False)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} - Habitación {self.habitacion.numero}"
+
+class RegistroConsumo(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name="registros")
+    consumo = models.FloatField() 
+    fecha = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return f"{self.dispositivo.get_tipo_display()} - {self.consumo} kWh - {self.fecha}"
+
+class Alerta(models.Model):
+    TIPOS_ALERTA = [
+        ('CONSUMO_ALTO', 'Consumo Alto'),
+        ('FALLA_SENSOR', 'Falla de Sensor'),
+        ('APAGADO_MANUAL', 'Apagado Manual'),
+        
+    ]
+
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE, related_name="alertas")
+    tipo = models.CharField(max_length=50, choices=TIPOS_ALERTA)
+    informacion = models.TextField()  # Detalles de la alerta
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Alerta {self.tipo} - {self.habitacion}"
