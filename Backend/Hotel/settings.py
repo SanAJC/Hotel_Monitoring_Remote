7a041dda57e0426c84460a1979a3caddb006df82
san ats <san@gmail.com>
2025-05-14 16:54:32 -0500

    Configuracion del Backend para Produccion Cors

--- Backend/Hotel/settings.py
+++ Backend/Hotel/settings.py
@@ -153,8 +153,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_CREDENTIALS = os.getenv('CORS_ALLOW_CREDENTIALS')
-CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS')
+# CORS Configuration
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173',os.getenv('FRONTEND_URL')]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
 ROOT_URLCONF = 'Hotel.urls'
 
