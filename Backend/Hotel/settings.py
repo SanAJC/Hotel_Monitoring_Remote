a805056cbfe4a2ade7bffe62ef61a939cb036300
san ats <san@gmail.com>
2025-05-15 11:11:01 -0500

    Configuracion para usar el volumen en railway

--- Backend/Hotel/settings.py
+++ Backend/Hotel/settings.py
@@ -274,8 +274,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #Media photos
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # -> /app/media en contenedor
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
